d2746585646d374c78ee3e0c5e3225d7db461215
Grzegorczyk Rafal <rgrzegorczyk@pretius.com>
2023-06-02 15:45:38 +0200

    DEMO 2 | Liquibase Standalone - only task-2 changes, status info, history info, part 3
    (cherry picked from commit d1aef51353373fbfd348af89cb2423f6705a8644)
    (cherry picked from commit 2258299bc9858ba467eaec50b569c53854d4577d)
    (cherry picked from commit 4ba7dcd454b8c75cbb796d427ecf0ca3465eeea1)
    (cherry picked from commit 4b5e4c1a8e3225d3e95d997ddd78a369916c7f8d)

---
+++ uat_history.sql
@@ -0,0 +1,18 @@
+Liquibase History for jdbc:oracle:thin:@rguat_low
+
+- Database updated at 02.06.2023, 13:28. Applied 11 changeset(s) in 8.262s, DeploymentId: 5712493309
+  sequence/departments_seq_sequence.sql::departments_seq_sequence::rafal
+  table/regions_table.sql::regions_table::rafal
+  table/departments_table.sql::departments_table::rafal
+  table/application_params_table.sql::application_params_table::rafal
+  table/countries_table.sql::countries_table::rafal
+  table/locations_table.sql::locations_table::rafal
+  view/emp_details_view_view.sql::emp_details_view_view::rafal
+  trigger/secure_countries_trigger.sql::secure_countries_trigger::rafal
+  trigger/secure_regions_trigger.sql::secure_regions_trigger::rafal
+  comment/regions_comments.sql::regions_comments::rafal
+  comment/countries_comments.sql::countries_comments::rafal
+
+- Database updated at 02.06.2023, 13:44. Applied 1 changeset(s), DeploymentId: 5713439600
+  procedure/secure_dml_procedure.sql::secure_dml_procedure::rafal
+
---
+++ uat_status.sql
@@ -0,0 +1,2 @@
+1 changesets have not been applied to RAFAL@jdbc:oracle:thin:@rguat_low
+     table/application_params_table.sql::application_params_table_add_col::rafal
