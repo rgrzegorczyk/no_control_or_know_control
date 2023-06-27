f8e9bcef8e2af668257a7b0b5556ff2f859a2934
Grzegorczyk Rafal <rgrzegorczyk@pretius.com>
2023-06-27 10:47:33 -0600

    Final fixes and presentation (new version)

--- demo/power_shell/remove_xml_files.ps1
+++ demo/power_shell/remove_xml_files.ps1
@@ -5,7 +5,7 @@ Let's prepare Power Shell Script
 
 I have a folder called database with some subfolders. Write power shell script that will remove all files with XML extension only from subfolders.
 I will execute script from database folder.
-Script can not delete XML files from database folder.
+Script will not delete XML files from database folder (we need controller.xml)
 
 #>
 Get-ChildItem -Path ".\*" -Include "*.xml" -Recurse | Where-Object { $_.DirectoryName -ne $pwd.Path -and $_.Name -ne "controller.xml" } | Remove-Item -Force
